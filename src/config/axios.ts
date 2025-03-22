c068f9a3c170c929e6a7eeccbeba541988607452
linhngocdao <linhdaongoc22@gmail.com>
2025-03-22 18:14:09 +0700

    update

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:8888/api/',
+  baseURL: 'http://144.172.97.18:8888/api',
   timeout: 160000,
   headers: {
     'Content-Type': 'application/json',
